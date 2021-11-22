256eddf9fed5673651e5e55ebc9a0886e1329a7a
Serega <26937163+serega-fighter@users.noreply.github.com>
2021-11-22 19:34:01 +0400

    Extract function

--- src/main/kotlin/com/serega/test/basic/HelloWorld.kt
+++ src/main/kotlin/com/serega/test/basic/HelloWorld.kt
@@ -3,9 +3,11 @@ package com.serega.test.basic
 import kotlinx.coroutines.*
 
 fun main() = runBlocking { // this: CoroutineScope
-    launch { // launch a new coroutine and continue
-        delay(1000L) // non-blocking delay for 1 second (default time unit is ms)
-        println("World!") // print after delay
-    }
-    println("Hello") // main coroutine continues while a previous one is delayed
+    launch { doWorld() }
+    println("Hello")
+}
+
+suspend fun doWorld() {
+    delay(1000L)
+    println("World!")
 }
