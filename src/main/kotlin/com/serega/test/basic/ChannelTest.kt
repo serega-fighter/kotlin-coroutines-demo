21d8c341d83b41ea8d014de4aa68bf17ac06b141
serega <26937163+serega-fighter@users.noreply.github.com>
2022-01-02 17:30:09 +0300

    Fan out channel test

--- src/main/kotlin/com/serega/test/basic/ChannelTest.kt
+++ src/main/kotlin/com/serega/test/basic/ChannelTest.kt
@@ -3,18 +3,27 @@ package com.serega.test.basic
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.*
+import kotlinx.coroutines.channels.ReceiveChannel
+
+fun CoroutineScope.launchProcessor(id: Int, channel: ReceiveChannel<Int>) = launch {
+    for (msg in channel) {
+        println("Processor #$id received $msg")
+    }
+}
 
 fun main() = runBlocking {
 
     val channel = Channel<Int>()
 
     launch {
-        for (x in 1..5) {
-            channel.send(x * x)
-            delay(1000)
+        var x = 1 // start from 1
+        while (isActive) {
+            channel.send(x++) // produce next
+            delay(10) // wait 0.1s
         }
     }
 
-    repeat(5) { println(channel.receive()) }
+    repeat(5) { launchProcessor(it, channel) }
+
     println("Done!")
 }
